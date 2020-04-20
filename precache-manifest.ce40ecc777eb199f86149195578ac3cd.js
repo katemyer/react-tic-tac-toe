1a0fc79d6124bde8e5329ff250923c8d699dbc99
kate myer <kate.d.mangubat@gmail.com>
2020-04-19 23:12:01 -0700

    Updates

---
+++ precache-manifest.ce40ecc777eb199f86149195578ac3cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50cf44c5a3c054aec0ed7fd093503393",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "c18734913758d85adad1",
+    "url": "/react-tic-tac-toe/static/css/main.7de2160c.chunk.css"
+  },
+  {
+    "revision": "27a0960a55eebee96846",
+    "url": "/react-tic-tac-toe/static/js/2.10758d43.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tic-tac-toe/static/js/2.10758d43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c18734913758d85adad1",
+    "url": "/react-tic-tac-toe/static/js/main.5bbd4960.chunk.js"
+  },
+  {
+    "revision": "bab408267c22d98e6389",
+    "url": "/react-tic-tac-toe/static/js/runtime-main.be4bbc4c.js"
+  }
+]);
